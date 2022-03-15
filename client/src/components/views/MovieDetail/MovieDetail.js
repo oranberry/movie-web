818acbaec357b7eb74ac2da85c0c94c31de1f916
Hyunjung Lee <leeleehj77@gmail.com>
2022-03-15 16:47:02 -0400

    Making Actors Grid

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
-import MainImage from '../LandingPage/Sections/MainImage';
+import MainImage from '../LandingPage/Sections/MainImage'
 import MovieInfo from './Sections/MovieInfo';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function MovieDetail(props) {
 
     let movieId = props.match.params.movieId
-
     const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [ActorToggle, setActorToggle] = useState(false)
+
 
     useEffect(() => {
 
         let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
-
         let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`
 
         fetch(endpointInfo)
@@ -22,7 +25,17 @@ function MovieDetail(props) {
                 setMovie(response)
             })
 
+        fetch(endpointCrew)
+            .then(response => response.json())
+            .then(response => {
+                setCasts(response.cast)
+            })
+
     }, [])
+
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle)
+    }
 
 
 
@@ -42,15 +55,27 @@ function MovieDetail(props) {
             <MovieInfo 
                 movie={Movie}
             />
-            
-
             <br />
-            {/* Actors Grid */}
 
+            {/* Actors Grid */}
             <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-                <button> Toggle Actor View </button>
+                <button onClick={toggleActorView}> Toggle Actor View </button>
             </div>
 
+            {ActorToggle && 
+                <Row gutter={[16, 16]}>
+                
+                    {Casts && Casts.map((cast, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                //props 넣어주기
+                                image={cast.profile_path ? `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                characterName={cast.name}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+            }
 
         </div>
 
