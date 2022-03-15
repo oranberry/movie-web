4b11679c57e22e7fd82e82ff12dfdfac57f7f2e0
Hyunjung Lee <leeleehj77@gmail.com>
2022-03-14 23:19:08 -0400

    Making Landing page and Grid Card Component

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+export const API_KEY = 'e92b0e3f1f8a0ee9dd718e3f7761755e';
